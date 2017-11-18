a07c3486dfdf6db25e7174a9f5e22ba995cd6ec8
AbdelhalimAhmed <abdelhalim.ahmed95@gmail.com>
2017-11-18 18:15:15 +0200

    add mime type to library

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
 import React from 'react';
 
-import {NativeModules} from 'react-native';
-export default NativeModules.ImageCropPicker;
+import {NativeModules, Platform} from 'react-native';
+const ImageCropPicker = NativeModules.ImageCropPicker;
+
+class DocumentPickerUtil {
+    static allFiles() {
+      return (Platform.OS === 'android') ? "*/*" : "public.content";
+    }
+
+    static images() {
+      return (Platform.OS === 'android') ? "image/*" : "public.image";
+    }
+
+    static plainText() {
+      return (Platform.OS === 'android') ? "text/plain" : "public.plain-text";
+    }
+
+    static audio() {
+      return (Platform.OS === 'android') ? "audio/*" : "public.audio";
+    }
+
+    static pdf() {
+      return (Platform.OS === 'android') ? "application/pdf" : "com.adobe.pdf";
+    }
+  }
+
+  module.exports = {DocumentPickerUtil, ImageCropPicker};
